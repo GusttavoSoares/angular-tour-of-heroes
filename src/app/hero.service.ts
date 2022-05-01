08fe37670847b17d5db0909797a6affbdaaf4a26
Gustavo Soares <gustavosoares335@yahoo.com.br>
2022-04-30 23:15:00 -0300

    Fazendo manipulação de erros.

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -4,6 +4,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { MessageService } from './message.service';
+import { catchError, map, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
@@ -15,8 +16,27 @@ export class HeroService {
     private messageService: MessageService) { }
 
 /** GET heroes from the server */
+/** GET heroes from the server */
 getHeroes(): Observable<Hero[]> {
   return this.http.get<Hero[]>(this.heroesUrl)
+    .pipe(
+      tap(_ => this.log('fetched heroes')),
+      catchError(this.handleError<Hero[]>('getHeroes', []))
+    );
+}
+
+private handleError<T>(operation = 'operation', result?: T) {
+  return (error: any): Observable<T> => {
+
+    // TODO: send the error to remote logging infrastructure
+    console.error(error); // log to console instead
+
+    // TODO: better job of transforming error for user consumption
+    this.log(`${operation} failed: ${error.message}`);
+
+    // Let the app keep running by returning an empty result.
+    return of(result as T);
+  };
 }
 
   getHero(id: number): Observable<Hero> {
